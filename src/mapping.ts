f84b6a1c9ce25985b48bbe6b79e7b22e53f47a27
felnao <38174160+felnao@users.noreply.github.com>
2020-12-04 14:26:57 +0100

    Add files via upload

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,58 +1,44 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { L2, Approval, Transfer } from "../generated/L2/L2"
-import { ExampleEntity } from "../generated/schema"
+import { Transferx, Approvalx } from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let id = event.params.owner
+            .toHexString()
+            .concat('-')
+            .concat(event.logIndex.toString())
+  
+  let Approvalx = new Approvalx(id)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+Approvalx.owner = event.params.owner
+Approvalx.spender = event.params.spender
+Approvalx.value = event.params.value
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+Approvalx.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.legacyToken(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  
 }
 
-export function handleTransfer(event: Transfer): void {}
+
+export function handleTransfer(event: Transfer): void {
+
+  let id = event.params.to
+            .toHexString()
+            .concat('-')
+            .concat(event.logIndex.toString())
+  
+  let Transferx = new Transferx(id)
+
+  
+Transferx.from = event.params.from
+Transferx.to = event.params.to
+Transferx.value = event.params.value
+
+
+Transferx.save()
+
+
+}
